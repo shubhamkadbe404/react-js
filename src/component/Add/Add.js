14da61df71c17b69f1d2b3b63df8179e51bbb178
SHUBHAM KADBE <swayam.kadbe@gmail.com>
2023-05-18 01:21:28 +0530

    second commit

--- src/component/Add/Add.js
+++ src/component/Add/Add.js
@@ -1,7 +1,7 @@
 const Add = (props) => {
     return(
         <div>
-            <h1>This is Addtion Function</h1>
+            <h1>This is the Addition Function</h1>
             <p>Addtion of 252 & 396 number's = {parseInt(props.a) + parseInt(props.b)}</p>
         </div>
     )
--- src/component/Sub/Sub.js
+++ src/component/Sub/Sub.js
@@ -1,7 +1,7 @@
 const Sub = (props) => {
     return(
         <div>
-            <h1>This is Substraction Function</h1>
+            <h1>This is the Substraction Function</h1>
             <p>Substraction of 2203 & 352 number's = {parseInt(props.a) - parseInt(props.b)}</p>
         </div>
     )
